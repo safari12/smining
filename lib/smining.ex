0440c9c49bae002afcfc12469fdcc6355c25954b
safari <safari@pop-os.localdomain>
2022-03-07 20:56:23 -0600

    initial commit

---
+++ lib/smining.ex
@@ -0,0 +1,18 @@
+defmodule Smining do
+  @moduledoc """
+  Documentation for Smining.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Smining.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/smining_test.exs
@@ -0,0 +1,8 @@
+defmodule SminingTest do
+  use ExUnit.Case
+  doctest Smining
+
+  test "greets the world" do
+    assert Smining.hello() == :world
+  end
+end
